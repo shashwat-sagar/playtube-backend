66221a2ac7c2c78b64ee8aec1aba280768390bc2
badass1901 <shashwatsagar19@gmail.com>
2024-02-07 00:52:46 +0530

    create user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const videoSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //using cloudinary
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
