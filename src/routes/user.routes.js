4135c8070667311b0b8b9989af1828f10f0fbc11
badass1901 <shashwatsagar19@gmail.com>
2024-02-15 14:36:55 +0530

    Change route method from POST to GET for /get-user endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -38,7 +38,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/get-user").post(verifyJWT, getCurrentUser);
+router.route("/get-user").get(verifyJWT, getCurrentUser);
 router.route("/update-details").patch(verifyJWT, updateAccountDetails);
 router
   .route("/update-avatar")
